eab3d412255ff54725d4a3efdb6d319c1be89a69
Davide Depau <davide@depau.eu>
2018-11-20 23:44:30 +0100

    Implement basic widget update mechanism in widget abstract base class

--- app/src/main/java/org/poul/bits/android/ui/widgets/HeadquartersStatusWidgetBase.kt
+++ app/src/main/java/org/poul/bits/android/ui/widgets/HeadquartersStatusWidgetBase.kt
@@ -2,38 +2,65 @@ package org.poul.bits.android.ui.widgets
 
 import android.appwidget.AppWidgetManager
 import android.appwidget.AppWidgetProvider
+import android.content.ComponentName
 import android.content.Context
+import android.content.Intent
 import android.widget.RemoteViews
 import org.poul.bits.R
+import org.poul.bits.android.broadcasts.BitsStatusReceivedBroadcast
+import org.poul.bits.android.model.BitsData
+import org.poul.bits.android.services.BitsRetrieveStatusService
 
-open class HeadquartersStatusWidgetBase : AppWidgetProvider() {
+
+abstract class HeadquartersStatusWidgetBase : AppWidgetProvider() {
+    abstract val layoutId: Int
 
     override fun onUpdate(context: Context, appWidgetManager: AppWidgetManager, appWidgetIds: IntArray) {
-        // There may be multiple widgets active, so update all of them
-        for (appWidgetId in appWidgetIds) {
+        // Fuck everything, we're doing it on our own
+        BitsRetrieveStatusService.startActionRetrieveStatus(context)
+    }
+
+    override fun onReceive(context: Context, intent: Intent) {
+        when (intent.action) {
+            BitsStatusReceivedBroadcast.ACTION ->
+                handleStatusReceivedBroadcast(context, intent)
+            else ->
+                super.onReceive(context, intent)
+        }
+    }
+
+    fun handleStatusReceivedBroadcast(context: Context, intent: Intent) {
+        val bitsData = intent.getParcelableExtra<BitsData>(BitsStatusReceivedBroadcast.BITS_DATA)
+        updateWidgets(context, bitsData)
+    }
+
+    fun updateWidgets(context: Context, bitsData: BitsData) {
+        val appWidgetManager = AppWidgetManager.getInstance(context)
+        val appWidgetIds = appWidgetManager.getAppWidgetIds(ComponentName(context, this::class.java))
+
+        appWidgetIds.forEach { appWidgetId ->
             updateAppWidget(
                 context,
                 appWidgetManager,
-                appWidgetId
+                appWidgetId,
+                bitsData
             )
         }
     }
 
-    companion object {
+    fun updateAppWidget(
+        context: Context,
+        appWidgetManager: AppWidgetManager,
+        appWidgetId: Int,
+        bitsData: BitsData
+    ) {
+        val widgetText = context.getString(R.string.appwidget_text)
+        // Construct the RemoteViews object
+        val views = RemoteViews(context.packageName, layoutId)
+        views.setTextViewText(R.id.appwidget_text, widgetText)
 
-        internal fun updateAppWidget(
-            context: Context, appWidgetManager: AppWidgetManager,
-            appWidgetId: Int
-        ) {
-
-            val widgetText = context.getString(R.string.appwidget_text)
-            // Construct the RemoteViews object
-            val views = RemoteViews(context.packageName, R.layout.headquarters_status_horizontal_widget)
-            views.setTextViewText(R.id.appwidget_text, widgetText)
-
-            // Instruct the widget manager to update the widget
-            appWidgetManager.updateAppWidget(appWidgetId, views)
-        }
+        // Instruct the widget manager to update the widget
+        appWidgetManager.updateAppWidget(appWidgetId, views)
     }
 }
 

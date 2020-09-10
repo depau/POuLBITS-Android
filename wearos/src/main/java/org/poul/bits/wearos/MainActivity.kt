0e3f13c0f92fbb7033e21100a709ab049fd7b846
Davide Depau <davide@depau.eu>
2020-09-10 22:34:15 +0200

    Support Wear OS

--- wearos/src/main/java/org/poul/bits/wearos/MainActivity.kt
+++ wearos/src/main/java/org/poul/bits/wearos/MainActivity.kt
@@ -1,15 +1,297 @@
 package org.poul.bits.wearos
 
+import android.annotation.SuppressLint
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
 import android.os.Bundle
 import android.support.wearable.activity.WearableActivity
+import android.text.Html
+import android.text.Spanned
+import android.text.format.DateUtils
+import android.view.View
+import eu.depau.kotlet.android.extensions.ui.view.snackbar
+import eu.depau.kotlet.extensions.builtins.round
+import kotlinx.android.synthetic.main.activity_main.*
+import org.poul.bits.android.lib.broadcasts.BitsStatusErrorBroadcast
+import org.poul.bits.android.lib.broadcasts.BitsStatusReceivedBroadcast
+import org.poul.bits.android.lib.controllers.appsettings.IAppSettingsHelper
+import org.poul.bits.android.lib.controllers.appsettings.enum.TemperatureUnit
+import org.poul.bits.android.lib.controllers.appsettings.impl.AppSettingsHelper
+import org.poul.bits.android.lib.misc.*
+import org.poul.bits.android.lib.model.BitsData
+import org.poul.bits.android.lib.model.BitsMessage
+import org.poul.bits.android.lib.model.BitsSensorData
+import org.poul.bits.android.lib.model.enum.BitsDataSource
+import org.poul.bits.android.lib.model.enum.BitsSensorType
+import org.poul.bits.android.lib.model.enum.BitsStatus
+import org.poul.bits.android.lib.mqtt_stub.MQTTHelperFactory
+import org.poul.bits.android.lib.services.BitsRetrieveStatusService
 
 class MainActivity : WearableActivity() {
 
+    private val bitsDataIntentFilter = IntentFilter(BitsStatusReceivedBroadcast.ACTION)
+    private val bitsErrorIntentFilter = IntentFilter(BitsStatusErrorBroadcast.ACTION)
+
+    private lateinit var appSettings: IAppSettingsHelper
+
+    private val bitsDataReceiver = object : BroadcastReceiver() {
+        override fun onReceive(context: Context, intent: Intent) {
+            when (intent.action) {
+                BitsStatusReceivedBroadcast.ACTION -> {
+                    updateGuiWithStatusData(intent.getParcelableExtra(BitsStatusReceivedBroadcast.BITS_DATA)!!)
+                    stopRefresh()
+                }
+                BitsStatusErrorBroadcast.ACTION -> {
+                    updateGuiError()
+                    status_button.snackbar(getString(R.string.check_network_connection))
+                    stopRefresh()
+                }
+            }
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        appSettings = AppSettingsHelper(this)
+        setAmbientEnabled()
         setContentView(R.layout.activity_main)
 
-        // Enables Always-on
-        setAmbientEnabled()
+        status_card.visibility = View.GONE
+        sensors_card.visibility = View.GONE
+        message_card.visibility = View.GONE
+
+        status_button.setOnClickListener {
+            playGialla()
+        }
+
+        settings_button.setOnClickListener {
+            startActivity(Intent(this, SettingsActivity::class.java))
+        }
+
+        swiperefreshlayout.setOnRefreshListener {
+            startRefresh()
+        }
+
+        startRefresh()
+    }
+
+    fun startRefresh() {
+        swiperefreshlayout.isRefreshing = true
+        BitsRetrieveStatusService.startActionRetrieveStatus(this)
+    }
+
+    fun stopRefresh() {
+        swiperefreshlayout.isRefreshing = false
+    }
+
+
+    fun updateGuiError() {
+        message_card.visibility = View.GONE
+        sensors_card.visibility = View.GONE
+        status_card.visibility = View.VISIBLE
+
+        status_card.text = Html.fromHtml(
+            SimpleHtml.italic(getString(R.string.status_retrieve_failure_card))
+        )
+
+        status_button.setBackgroundColor(resources.getColor(R.color.colorHQsGialla, theme))
+        status_button.text = getString(R.string.headquarters_gialla)
+    }
+
+    fun updateGuiWithStatusData(bitsData: BitsData) {
+        if (bitsData.status != null) {
+            status_button.text = getTextForStatus(bitsData.status!!)
+            status_button.setBackgroundColor(
+                resources.getColor(
+                    getColorForStatus(bitsData.status!!),
+                    theme
+                )
+            )
+
+            if (bitsData.source != BitsDataSource.MQTT)
+                updateStatusCardWithStatusData(bitsData)
+        }
+
+        if (bitsData.source != BitsDataSource.MQTT)
+            updateMessageCardWithMessage(bitsData)
+
+        updateSensorCardWithTempData(bitsData)
+
+        // Refresh because MQTT data doesn't have a lot of fields
+        if (bitsData.source == BitsDataSource.MQTT)
+            startRefresh()
+    }
+
+    fun updateStatusCardWithStatusData(bitsData: BitsData) {
+        status_card.visibility = View.VISIBLE
+
+        if (bitsData.lastModified != null) {
+            val text = getStatusCardText(this, bitsData)
+            status_card.text = text
+        }
+    }
+
+    private fun getSensorValueWithUserPreferredUnit(reading: BitsSensorData): Double =
+        when (reading.type!!) {
+            BitsSensorType.TEMPERATURE ->
+                when (appSettings.temperatureUnit) {
+                    TemperatureUnit.CELSIUS -> reading.value
+                    TemperatureUnit.FAHRENHEIT -> celsiusToFahrenheit(reading.value)
+                    TemperatureUnit.KELVIN -> celsiusToKelvin(reading.value)
+                }
+            BitsSensorType.HUMIDITY ->
+                reading.value
+        }
+
+    private fun getUserPreferredUnitStringForSensorReading(reading: BitsSensorData): String =
+        when (reading.type!!) {
+            BitsSensorType.TEMPERATURE ->
+                when (appSettings.temperatureUnit) {
+                    TemperatureUnit.CELSIUS -> "°C"
+                    TemperatureUnit.FAHRENHEIT -> "°F"
+                    TemperatureUnit.KELVIN -> "K"
+
+                }
+            BitsSensorType.HUMIDITY ->
+                "%"
+        }
+
+    @SuppressLint("SetTextI18n")
+    fun updateSensorCardWithTempData(bitsData: BitsData) {
+        val sensorData = bitsData.sensors ?: return
+
+        sensors_card.visibility = if (sensorData.isEmpty()) View.GONE else View.VISIBLE
+
+        sensorDataLoop@ for (reading in sensorData) {
+            val view = when (reading.type) {
+                BitsSensorType.TEMPERATURE -> temperature_textview
+                BitsSensorType.HUMIDITY -> humidity_textview
+                null -> continue@sensorDataLoop
+            }
+
+            val value = getSensorValueWithUserPreferredUnit(reading).round(1)
+            val unit = getUserPreferredUnitStringForSensorReading(reading)
+
+            view.text = "$value$unit"
+            view.visibility = View.VISIBLE
+        }
+    }
+
+    fun updateMessageCardWithMessage(bitsData: BitsData) {
+        val msgData = bitsData.message ?: return
+
+        if (msgData.empty) {
+            message_card.visibility = View.GONE
+            return
+        } else {
+            message_card.visibility = View.VISIBLE
+        }
+
+        message_card.text = getMessageCardText(this, msgData)
+    }
+
+    fun optionallyStartStopMqttService() {
+        if (!appSettings.mqttEnabled)
+            return stopMqttService()
+
+        MQTTHelperFactory.getMqttHelper(appSettings).startService(this)
+    }
+
+    fun stopMqttService() {
+        MQTTHelperFactory.getMqttHelper(appSettings).stopService(this)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        registerReceiver(bitsDataReceiver, bitsDataIntentFilter)
+        registerReceiver(bitsDataReceiver, bitsErrorIntentFilter)
+        optionallyStartStopMqttService()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        unregisterReceiver(bitsDataReceiver)
+        stopMqttService()
+    }
+
+    companion object {
+        fun getStatusCardText(context: Context, bitsData: BitsData): Spanned? {
+            bitsData.lastModified ?: return null
+            bitsData.modifiedBy ?: return null
+
+            val openedclosed = SimpleHtml.esc(
+                context.getString(
+                    when (bitsData.status) {
+                        BitsStatus.OPEN -> R.string.opened_from
+                        BitsStatus.CLOSED -> R.string.closed_from
+                        else -> R.string.headquarters_gialla
+                    }
+                )
+            )
+
+            val changedTime = SimpleHtml.esc(
+                DateUtils.getRelativeTimeSpanString(
+                    bitsData.lastModified!!.time,
+                    System.currentTimeMillis(),
+                    0L,
+                    DateUtils.FORMAT_ABBREV_ALL
+                ) as String
+            )
+
+            return Html.fromHtml(
+                "$openedclosed ${
+                    SimpleHtml.bold(
+                        SimpleHtml.color(
+                            context,
+                            SimpleHtml.esc(bitsData.modifiedBy!!),
+                            R.color.colorAccent
+                        )
+                    )
+                }<br>" +
+                        "${context.getString(R.string.last_changed)} ${
+                            SimpleHtml.bold(
+                                SimpleHtml.color(
+                                    context,
+                                    changedTime,
+                                    R.color.colorAccent
+                                )
+                            )
+                        }"
+            )!!
+        }
+
+        fun getMessageCardText(context: Context, bitsData: BitsMessage): Spanned {
+            val sentTime = SimpleHtml.esc(
+                DateUtils.getRelativeTimeSpanString(
+                    bitsData.lastModified.time,
+                    System.currentTimeMillis(),
+                    0L,
+                    DateUtils.FORMAT_ABBREV_ALL
+                ) as String
+            )
+
+            return Html.fromHtml(
+                "${context.getString(R.string.last_msg_from)} ${
+                    SimpleHtml.bold(
+                        SimpleHtml.color(
+                            context,
+                            SimpleHtml.esc(bitsData.user),
+                            R.color.colorAccent
+                        )
+                    )
+                }, ${
+                    SimpleHtml.bold(
+                        SimpleHtml.color(
+                            context,
+                            SimpleHtml.esc(sentTime),
+                            R.color.colorAccent
+                        )
+                    )
+                } ${SimpleHtml.br}" +
+                        SimpleHtml.italic("“${SimpleHtml.esc(bitsData.message)}”")
+            )!!
+        }
     }
 }
